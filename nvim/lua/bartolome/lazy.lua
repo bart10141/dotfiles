955ea00172ba73ca56299674a98cad2f54eeb766
bart10141 <98976406+bart10141@users.noreply.github.com>
2024-10-30 18:43:10 +0100

    LSP y linter configurado nvim

--- nvim/lua/bartolome/lazy.lua
+++ nvim/lua/bartolome/lazy.lua
@@ -22,8 +22,8 @@ vim.opt.rtp:prepend(lazypath)
 -- Setup lazy.nvim
 require("lazy").setup({
   spec = {
-    -- import your plugins
     { import = "bartolome.plugins" },
+    { import = "bartolome.plugins.lsp" },
   },
   -- automatically check for plugin updates
   checker = { enabled = false },
